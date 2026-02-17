28b15aeaae2299873d885aedb5b7f8cb2d865b2d
Paweł <pawel@msd.info.pl>
2026-02-17 13:21:39 +0100

    Zdarzenia

---
+++ ConsoleApp.Tests.xUnit/FluentAssertions/LoggerTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace ConsoleApp.Tests.xUnit.FluentAssertions
+{
+    public class LoggerTests
+    {
+        [Fact]
+        public void Log_AnyInput_ValidLoggerEventArgs()
+        {
+            //Arrange
+            var logger = new Logger();
+            var log = new Fixture().Create<string>();
+            object? sender = null;
+            Logger.LoggerEventArgs? eventArgs = null;
+            logger.MessageLogged += (s, e) => { sender = s; eventArgs = e; };
+
+
+            //Act
+            logger.Log(log);
+
+            //Assert
+            using (new AssertionScope())
+            {
+                sender.Should().NotBeNull().And.Be(logger);
+                eventArgs.Should().NotBeNull();
+                eventArgs.Message.Should().Be(log);
+                eventArgs.Timestamp.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+            }
+        }
+
+        [Fact]
+        public void Log_AnyInput_ValidLoggerEventArgs_FA()
+        {
+            //Arrange
+            var logger = new Logger();
+            var log = new Fixture().Create<string>();
+            using var monitor = logger.Monitor();
+
+            //Act
+            logger.Log(log);
+
+            //Assert
+            using (new AssertionScope())
+            {
+                monitor.Should().Raise(nameof(logger.MessageLogged))
+                    .WithSender(logger)
+                    .WithArgs<Logger.LoggerEventArgs>(e => e.Message == log);
+            }
+        }
+    }
+}
---
+++ ConsoleApp.Tests.xUnit/LoggerTests.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture;
+
+namespace ConsoleApp.Tests.xUnit
+{
+    public class LoggerTests
+    {
+
+        [Fact]
+        public void Log_AnyInput_EventInvokedOnce()
+        {
+            //Arrange
+            var logger = new Logger();
+            var log = new Fixture().Create<string>();
+            var eventInvoked = 0;
+            logger.MessageLogged += (_, _) => eventInvoked++;
+            const int EXPECTED_RESULT = 1;
+
+            //Act
+            logger.Log(log);
+
+            //Assert
+            Assert.Equal(EXPECTED_RESULT, eventInvoked);
+        }
+
+        [Fact]
+        public void Log_AnyInput_ValidLoggerEventArgs()
+        {
+            //Arrange
+            var logger = new Logger();
+            var log = new Fixture().Create<string>();
+            object? sender = null;
+            Logger.LoggerEventArgs? eventArgs = null;
+            logger.MessageLogged += (s, e) => { sender = s; eventArgs = e; };
+
+
+            //Act
+            var dateTimeStart = DateTime.Now;
+            logger.Log(log);
+            var dateTimeEnd = DateTime.Now;
+
+            //Assert
+            Assert.NotNull(sender);
+            Assert.NotNull(eventArgs);
+            Assert.Equal(logger, sender);
+            Assert.Equal(log, eventArgs.Message);
+            Assert.InRange(eventArgs.Timestamp, dateTimeStart, dateTimeEnd);
+        }
+
+    }
+}
